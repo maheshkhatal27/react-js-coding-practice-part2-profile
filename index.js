7f5b059c5f58763900e1be8366af07e0caf330c5
maheshNxtwave <mbkhatal@gmail.com>
2022-09-13 16:52:28 +0000

    coding practice 1-p2

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div class="main-bg-container">
+    <h1 class="main-heading">Congratulations</h1>
+    <div class="profile-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="profile-pic"
+      />
+      <h1 class="name">Kiran V</h1>
+      <p class="desc">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="watch-pic"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
